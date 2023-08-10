b534ff8b06b32d427fa1a35478244ac61f680e4e
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-08-10 13:44:15 +0700

    add

--- server.go
+++ server.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("METHOD: ", r.Method)
+	// get query param from http://localhost:8080/users/:id?name=nong
+	// get query param from http://localhost:8080/users/:id
+	name := r.URL.Query().Get("name")
+	fmt.Println("name:", name)
+
+	if r.Method == "POST" {
+		w.Write([]byte("hello"))
+		return
+	}
+
 	if r.Method == "GET" {
 		w.Write([]byte("Hello, world!"))
 		return
@@ -19,5 +29,7 @@ func main() {
 	log.Println("Starting server on port 8080")
 
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/hello/{id}", helloHandler)
+	// SELECT * FROM hello WHERE id = {id}
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
