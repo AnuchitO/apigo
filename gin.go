ade8634720b08dad9c79fae71bb9ca6e74ce7042
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-08-11 15:39:32 +0700

    add

--- gin.go
+++ gin.go
@@ -108,6 +108,8 @@ curl -X POST -H "Content-Type: application/json" -d '{"amount": 100.0}' http://l
 */
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	router := newServer()
 
 	srv := &http.Server{
@@ -115,28 +117,30 @@ func main() {
 		Handler: router,
 	}
 
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-
-	// Start listening for shutdown signals
+	// Start the server in a goroutine
 	go func() {
-		log.Println("waiting..")
-		<-shutdown
-		// Close database connections, channels, etc.
-		log.Println("Shutting down server...")
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		if err := srv.Shutdown(ctx); err != nil {
-			log.Fatalf("Server shutdown error: %v", err)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error: %v", err)
 		}
-		log.Println("Shutting down gracefully...")
-		os.Exit(0)
 	}()
 
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("Error: %v", err)
+	// Listen for shutdown signals
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Server is shutting down...")
+
+	// Give the server a grace period to finish ongoing requests
+	const graceTimeout = 5 * time.Second
+	ctx, cancel = context.WithTimeout(ctx, graceTimeout)
+	defer cancel()
+
+	// Shut down the server gracefully
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown error: %v", err)
 	}
+
+	log.Println("Server gracefully stopped")
 }
 
 // new Server return Gin
